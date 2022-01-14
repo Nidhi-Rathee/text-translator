50d02fdb10c07bbb3d76321bad26c66b8971ff5d
Nidhi Rathee <nidhi.rathee@oneadvanced.com>
2022-01-14 20:54:40 +0530

    feat: configures firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Box, Select, Button, Textarea, Text, Image } from '@chakra-ui/react'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
+import { db } from './firebase'
+import { collection, addDoc, getDocs } from 'firebase/firestore'
 
 function App() {
     const [options, setOptions] = useState([])
@@ -11,7 +13,7 @@ function App() {
 
     useEffect(() => {
         getLanguages()
-    })
+    }, [])
 
     const getLanguages = () => {
         axios.get('https://libretranslate.de/languages',
@@ -21,53 +23,59 @@ function App() {
             })
     }
 
-    const translate = () => {
-
+    const translate = async () => {
         let data = {
             q: input,
             source: from,
             target: to,
         }
 
-        axios.post('https://libretranslate.de/translate', data).then(res => {
+
+        axios.post('https://libretranslate.de/translate', data).then(async (res) => {
             console.log('post', res.data)
             setOutput(res.data.translatedText)
+            const dataRef = collection(db, 'words')
+            await addDoc(dataRef, { from: { wordFrom: input, wordTo: res.data } })
         })
+
     }
 
     return (
-        <Box className="App" h='100vh' justifyContent='center' alignItems='center'>
-            <Box >
-                <Box d='flex' alignItems='center' justifyContent='center'>
-                    <Image w='10%' src='favicon.ico' alt='Dan Abramov' borderRadius='lg' />
-                    <Text p={[2, 4, 6, 8]} fontSize='2xl' textAlign='center' fontWeight='4xl' fontFamily='system-ui'> Text Translator</Text>
-                </Box>
-                <Box d='flex' flexDirection='column' alignItems='center'>
-                    <Box width='50%'>
-                        <Text p={2} fontSize='1xl' fontFamily='system-ui'>From: {from}</Text>
-                        <Select placeholder='Select language' onChange={e => setFrom(e.target.value)}>
-                            {options.map(opt => <option key={opt.code} value={opt.code}>{opt.name}</option>)}
-                        </Select>
+        <>
+            <Box className="App" >
+                <Box d='flex' flexDirection='column'>
+                    <Box d='flex' alignItems='center' justifyContent='center'>
+                        <Image w='10%' src='favicon.ico' alt='Dan Abramov' borderRadius='lg' />
+                        <Text p={[2, 4, 6, 8]} fontSize='2xl' textAlign='center' color='teal' fontWeight='4xl' fontFamily='system-ui'> Text Translator</Text>
                     </Box>
-                    <Box width='50%'>
-                        <Text p={2} fontSize='1xl' fontFamily='system-ui'>To: {to}</Text>
-                        <Select placeholder='Select language' onChange={e => setTo(e.target.value)}>
-                            {options.map(opt => <option key={opt.code} value={opt.code}>{opt.name}</option>)}
-                        </Select>
-                    </Box>
-                    <Box width='50%' mt={5}>
-                        <Textarea placeholder='Here is a sample placeholder' onInput={e => setInput(e.target.value)} />
-                    </Box>
-                    <Box width='50%' mt={5}>
-                        <Textarea placeholder='Here is a sample placeholder' value={output} />
-                    </Box>
-                    <Box mt={2}>
-                        <Button backgroundColor='teal' color='white' onClick={e => translate()}>Translate</Button>
-                    </Box>
-                </Box>
-            </Box>
+                    <Box m={10} justifyContent='center' alignItems='center'>
+                        <Box >
+                            <Text fontSize='1xl' fontFamily='system-ui' color='teal'>From: {from}</Text>
+                            <Select placeholder='Select language' onChange={e => setFrom(e.target.value)}>
+                                {options.map(opt => <option key={opt.code} value={opt.code}>{opt.name}</option>)}
+                            </Select>
+                        </Box>
+                        <Box >
+                            <Text fontSize='1xl' fontFamily='system-ui' color='teal'>To: {to}</Text>
+                            <Select placeholder='Select language' onChange={e => setTo(e.target.value)}>
+                                {options.map(opt => <option key={opt.code} value={opt.code}>{opt.name}</option>)}
+                            </Select>
+                        </Box>
+                        <Box mt={5} >
+                            <Textarea placeholder='Here is a sample placeholder' id='inputText' onInput={e => setInput(e.target.value)} />
+                        </Box>
+                        <Box mt={5} d='flex' justifyContent='center'>
+                            <Button backgroundColor='teal' color='white' onClick={e => translate()}>Translate</Button>
+                        </Box>
+                        <Box mt={5}>
+                            <Textarea placeholder='Here is a sample placeholder' id='outputText' value={output} />
+                        </Box>
 
-        </Box>
+                    </Box>
+                </Box>
+
+            </Box>
+        </>
     );
 }
 
